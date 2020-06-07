a9a5b8ba1e3869672748f02e2b1c4cb93b35af1e
kazamakazuki1724012 <65148345+kazamakazuki1724012@users.noreply.github.com>
2020-06-07 21:25:36 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 int main()
 {
-  for (int i = 1; i <= 1000; i++)
+  for (int i = 1; i <= 100; i++)
   {
     if ((i % 15) == 0)
-      cout << "FizzBuzz,";
+      cout << "FizzBuzz,\n";
     else if ((i % 3) == 0)
-      cout << "Fizz,";
+      cout << "Fizz,\n";
     else if ((i % 5) == 0)
-      cout << "Buzz,";
+      cout << "Buzz,\n";
     else
-      cout << i << ",";
+      cout << i << ",\n";
   }
   return 0;
 }
