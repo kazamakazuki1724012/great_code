913cfa84eef194b07fb7a12a15215e2ec4f23f88
kazamakazuki1724012 <65148345+kazamakazuki1724012@users.noreply.github.com>
2020-06-07 21:11:32 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -7,13 +7,13 @@ int main()
   for (int i = 0; i <= 1000; i++)
   {
     if ((i % 15) == 0)
-      cout << "FizzBuzz" << endl;
+      cout << "FizzBuzz" << flush;
     else if ((i % 3) == 0)
-      cout << "Fizz" << endl;
+      cout << "Fizz" << flush;
     else if ((i % 5) == 0)
-      cout << "Buzz" << endl;
+      cout << "Buzz" << flush;
     else
-      cout << i << endl;
+      cout << i << flush;
   }
   return 0;
 }
